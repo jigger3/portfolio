276097f1ea823185c41009330170fad25762448a
jigger3 <160100621+jigger3@users.noreply.github.com>
2025-04-23 12:05:42 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,7 @@
+var typed = new Typed(".text", {
+    strings: ["Software Developer", "Problem Solver", "Software Tester", "Algorithm Expert", "System Architect"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+});
